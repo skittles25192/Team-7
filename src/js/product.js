351994b78ef81e03b6c5d680ad6492e6148c0610
Hunter Perry <skittles2519@hotmail.com>
2023-05-03 09:45:14 -0600

    Week 3 Team pull request commit

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,7 @@
-import { setLocalStorage, getLocalStorage } from './utils.mjs';
-import { findProductById } from './productData.mjs';
+import {getParameter} from "./utils.mjs";
+import productDetails from "./productDetails.mjs";
 
-function addProductToCart(product) {
-  const currentCart = getLocalStorage('so-cart') || [];
-  const updatedCart = [...currentCart, product];
-  setLocalStorage('so-cart', updatedCart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const productId = getParameter("product");
+productDetails(productId);
 
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+console.log(productId)
