9d964a426ca85493e0c047253f34e07f6927697f
nelsonpetro <nelsonpetrodoria@gmail.com>
2023-05-10 09:53:20 -0500

    Week 4 team assignment

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,5 @@
-import { getParameter } from './utils.mjs';
-import productDetails from './productDetails.mjs';
+import { getParameter } from "./utils.mjs";
+import productDetails from "./productDetails.mjs";
 
-const productId = getParameter('product');
+const productId = getParameter("product");
 productDetails(productId);
-
