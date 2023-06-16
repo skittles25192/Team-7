f6100d6a40faea220bef9515ea759b5b85721700
nelsonpetro <nelsonpetrodoria@gmail.com>
2023-06-16 17:46:48 -0500

    Changes made for the chec login function

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,11 @@ import { loadHeaderFooter } from './utils.mjs';
 import { updateCartCount } from './superscript.mjs';
 import { checkLogin } from './auth.mjs';
 
+checkLogin();
+
 loadHeaderFooter()
   .then(() => {
     updateCartCount();
-    checkLogin();
   })
   .catch((error) => {
     alert('Error loading header and footer: ' + error);
